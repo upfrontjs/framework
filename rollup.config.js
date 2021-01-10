ec980d619f588b2223e44203668dbde78aaa463c
Nandor Kraszlan <95nandi95@gmail.com>
2021-01-10 21:13:25 +0000

    Improved documentation, worked on publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
 import typescript from "@rollup/plugin-typescript";
 import pkg from './package.json';
+import * as tsConfig from './tsconfig.json';
 import { terser } from "rollup-plugin-terser";
 import bundleSize from 'rollup-plugin-bundle-size';
 import copy from 'rollup-plugin-copy'
+
+const sourcemapPathTransform = (relativeSourcePath, sourcemapPath) => {
+    // fix the relative path which was pointing to the original src,
+    // and point to the copied in src folder
+    return '.' + relativeSourcePath.slice(tsConfig.compilerOptions.sourceRoot.length)
+}
 
 export default {
     input: 'src/index.ts',
@@ -10,12 +17,14 @@ export default {
         {
             file: pkg.main,
             format: 'cjs',
-            sourcemap: true
+            sourcemap: true,
+            sourcemapPathTransform
         },
         {
             file: pkg.module,
             format: 'es',
-            sourcemap: true
+            sourcemap: true,
+            sourcemapPathTransform
         }
     ],
     external: [
@@ -27,7 +36,7 @@ export default {
         bundleSize(),
         copy({
             targets: [
-                { src: ['LICENSE.txt', 'docs/README.md'], dest: 'lib' },
+                { src: ['LICENSE.txt', 'docs/README.md', 'src'], dest: 'lib' },
             ],
             copyOnce: true
         })

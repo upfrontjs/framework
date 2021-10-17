caf1f75304f5cc6d8b98c3451cfc9bfe4edd593b
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-10-17 10:06:21 +0100

    feat: added ExtractArguments and DeepPartial type utility types

--- src/Support/type.ts
+++ src/Support/type.ts
@@ -11,3 +11,18 @@ export type RequireSome<T extends Record<PropertyKey, any>, K extends keyof T> =
  * Make the type either the initial value or an array of it.
  */
 export type MaybeArray<T> = T | T[];
+
+/**
+ * Get the type of the arguments for the given callable type into a tuple.
+ */
+export type ExtractArguments<T> = [T] extends [(...args: infer U) => any]
+    ? U
+    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+    : [T] extends [void] ? [] : [T];
+
+/**
+ * Set every property nested or otherwise to optional.
+ */
+export type DeepPartial<T> = {
+    [P in keyof T]?: DeepPartial<T[P]> | T[P];
+};

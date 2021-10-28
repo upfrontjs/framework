77af5538d84a65ee25f6a908bd33f33a32c73661
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-10-28 15:21:04 +0100

    style: ignored eslint issues

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -4,11 +4,13 @@ import type Configuration from '../src/Contracts/Configuration';
 
 const config: GlobalConfig<Configuration> = new GlobalConfig;
 
+// eslint-disable-next-line jest/require-hook
 fetchMock.enableMocks();
 jest.useFakeTimers('modern');
 const now = new Date(jest.getRealSystemTime());
 
-global.beforeEach(() => {
+// eslint-disable-next-line jest/require-top-level-describe
+beforeEach(() => {
     config.reset();
     config.set('baseEndPoint', 'https://test-api-endpoint.com');
     jest.setSystemTime(now);

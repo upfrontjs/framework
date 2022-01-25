upfrontjs/framework

commit b4d6db429f451107419d4bb48c95d2b165036b2e

refactor(model-collection): updated `toJSON` typing
updated `toJSON` typing to track the model's `toJSON`

src/Calliope/ModelCollection.ts
@@ -418,7 +418,7 @@ export default class ModelCollection<T extends Model> extends Collection<T> {
     /**
      * @inheritDoc
      */
-    public override toJSON(): { elements: ReturnType<typeof JSON.parse>[] } {
+    public override toJSON(): { elements: Model['toJSON'][] } {
         // eslint-disable-next-line max-len
         // https://security.stackexchange.com/questions/7001/how-should-web-app-developers-defend-against-json-hijacking/7003#7003
         return { elements: this.toArray().map(model => model.toJSON()) };

commit 10fd04a8d04cddb414110967d5220d0eb9fce08b

fix(model-collection): fixed wrong typing of `toJSON`
Issue introduced in b4d6db429f451107419d4bb48c95d2b165036b2e

src/Calliope/ModelCollection.ts
@@ -418,7 +418,7 @@ export default class ModelCollection<T extends Model> extends Collection<T> {
     /**
      * @inheritDoc
      */
-    public override toJSON(): { elements: Model['toJSON'][] } {
+    public override toJSON(): { elements: ReturnType<T['toJSON']>[] } {
         // eslint-disable-next-line max-len
         // https://security.stackexchange.com/questions/7001/how-should-web-app-developers-defend-against-json-hijacking/7003#7003
         return { elements: this.toArray().map(model => model.toJSON()) };
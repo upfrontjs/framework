a4bec5bad76a8637f6316c493d7c9bb3b9339d91
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2022-02-09 19:31:00 +0000

    fix(internal): prevent losing error name when mangling code

--- src/Exceptions/LogicException.ts
+++ src/Exceptions/LogicException.ts
@@ -1,4 +1,7 @@
 import BaseException from './BaseException';
 
 export default class LogicException extends BaseException {
+    public get name(): string {
+        return 'LogicException';
+    }
 }

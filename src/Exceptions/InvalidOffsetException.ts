2b02f37f3ab98800b8ebd3c02abffc0c7185fe49
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2023-01-31 23:58:18 +0000

    docs(internal): commented exception classes

--- src/Exceptions/InvalidOffsetException.ts
+++ src/Exceptions/InvalidOffsetException.ts
@@ -1,5 +1,8 @@
 import BaseException from './BaseException';
 
+/**
+ * Error that occurs when accessing an unexpected property on a structure.
+ */
 export default class InvalidOffsetException extends BaseException {
     public get name(): string {
         return 'InvalidOffsetException';
--- src/Exceptions/LogicException.ts
+++ src/Exceptions/LogicException.ts
@@ -1,5 +1,8 @@
 import BaseException from './BaseException';
 
+/**
+ * Error that occurs when trying to execute logic that is not supported.
+ */
 export default class LogicException extends BaseException {
     public get name(): string {
         return 'LogicException';

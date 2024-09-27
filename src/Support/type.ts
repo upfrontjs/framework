d2531e305cc0194dc6a2798df8a9ae24dc3406ca
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2024-09-27 21:00:04 +0100

    perf: use built in utility types

--- src/Support/type.ts
+++ src/Support/type.ts
@@ -4,16 +4,12 @@ import type Model from '../Calliope/Model';
 /**
  * Make the properties defined in the union required.
  */
-export type RequireSome<T extends Record<PropertyKey, any>, K extends keyof T> = Omit<T, K> & {
-    [MK in K]-?: NonNullable<T[MK]>
-};
+export type RequireSome<T extends Record<PropertyKey, any>, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
 
 /**
  * Make the properties defined in the union optional.
  */
-export type PartialSome<T extends Record<PropertyKey, any>, K extends keyof T> = Omit<T, K> & {
-    [MK in K]?: T[MK]
-};
+export type PartialSome<T extends Record<PropertyKey, any>, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 /**
  * Make the type either the initial value or an array of it.

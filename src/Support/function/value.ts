eee24e772b5bf3b1d82229c1572fd01097d5975f
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2025-09-22 11:20:49 +0100

    fix(helpers): fix type issue

--- src/Support/function/value.ts
+++ src/Support/function/value.ts
@@ -1,3 +1,3 @@
 export default function value<T>(val: T, ...args: any[]): T extends (...args: any[]) => infer R ? R : T {
-    return typeof val === 'function' ? val(...args) : val;
+    return typeof val === 'function' ? val(...args) : val as any;
 }
